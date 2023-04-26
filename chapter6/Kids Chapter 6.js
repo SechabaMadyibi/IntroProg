0890e5fd9881ab046d6219e38cabdee5ae452e5a
SechabaMadyibi <100441327+SechabaMadyibi@users.noreply.github.com>
2023-04-26 15:28:22 +0200

    Add files via upload

---
+++ chapter6/Kids Chapter 6.js
@@ -0,0 +1,13 @@
+let lemonChicken = false;
+let beefWithBlackBean = false;
+let sweetAndSourPork = false;
+
+if (lemonChicken) {
+ console.log("Great! I'm having lemon chicken!");
+} else if (beefWithBlackBean) {
+ console.log("I'm having the beef.");
+} else if (sweetAndSourPork) {
+ console.log("OK, I'll have the pork.");
+} else {
+ console.log("Well, I guess I'll have rice then.");
+}
